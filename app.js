aa1e07b3f1432e7590c6c73c5db30049d0cc22f2
Chris Chock <chris.chock@polaris.com>
2019-10-04 13:12:37 -0500

    Fix swapped res and req

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get('/teapot', (req, res) => {
+	req.set('X-teapot', 'I\'m a teapot');
+	req.sendStatus(418);
 });
 
-app.get('/teapot', (req, res) => {
-	res.set('X-teapot', 'I\'m a teapot');
-	res.sendStatus(418);
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
 
 app.listen(port, () => console.log(`Example app listening on poart ${port}!`));
